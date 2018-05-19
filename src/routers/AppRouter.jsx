d7e1c990469266ede56cb452a783be19dee57102
Christoph Flick <christophflick@gmx.de>
2018-05-19 20:55:47 +0200

    Fixed EditPost routing

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
 import AddPostPage from '../components/editor/AddPostPage';
+import EditPostPage from '../components/editor/EditPostPage';
 import DashboardPage from '../components/DashboardPage';
 import EditorDashboardPage from '../components/editor/DashboardPage';
 import LoginPage from '../components/LoginPage';
@@ -14,17 +15,15 @@ export const history = createHistory();
 
 const AppRouter = () => (
     <Router history={history}>
-        <div>
-            <Switch>
-                <PublicRoute path="/" component={DashboardPage} exact={true} />
-                <PublicRoute path="/dashboard" component={DashboardPage} />
-                <PublicRoute path="/login" component={LoginPage} />
-                <PrivateRoute path="/editor/dashboard" component={EditorDashboardPage} />
-                <PrivateRoute path="/editor/add" component={AddPostPage} />
-                <PrivateRoute path="/editor/edit/:id" component={AddPostPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
+        <Switch>
+            <PublicRoute path="/" component={DashboardPage} exact={true} />
+            <PublicRoute path="/dashboard" component={DashboardPage} />
+            <PublicRoute path="/login" component={LoginPage} />
+            <PrivateRoute path="/editor/dashboard" component={EditorDashboardPage} />
+            <PrivateRoute path="/editor/add" component={AddPostPage} />
+            <PrivateRoute path="/editor/post/:id" component={EditPostPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
     </Router>
 );
 
