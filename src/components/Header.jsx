ec7e563d76661863eb9f55fd08225d855c0bfd17
Christoph Flick <christophflick@gmx.de>
2018-05-28 22:40:29 +0200

    Added style to the header of the visitor page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,61 @@ import { connect } from 'react-redux';
 
 import { startLogout } from '../actions/auth';
 
+const DEFAULT_HEADER_CLASSES = 'header header--fixed-top';
 
-export const Header = ({ isAuthenticated, startLogout }) => {
-    return (
-        <header className="header">
-            <div className="content-container">
-                <div className="header__content">
-                    <Link className="header__title" to="/dashboard">
-                        <h1>Blogstoph</h1>
-                    </Link>
-                    {isAuthenticated ? (
-                        <div>
-                            <Link className="button button--link" to="/editor/dashboard">Editor Area</Link>
-                            <button className="button button--link" onClick={startLogout}>Logout</button>
-                        </div>
-                    ) : (
-                        <Link className="button button--link" to="/login">Login</Link>
-                    )}
+export class Header extends React.Component {
+    state = {
+        headerClasses: DEFAULT_HEADER_CLASSES
+    }
+
+    componentDidMount = () => {
+        window.addEventListener('scroll', this.handleScroll);
+    };
+
+    componentWillUnmount = () => {
+        window.removeEventListener('scroll', this.handleScroll);
+    };
+
+    handleScroll = () => {
+        const scrollPosPrev = this.state.scrollPos;
+        const scrollPos = window.scrollY;
+        let headerClasses = 'header header--fixed-top';
+
+        if (scrollPos > 80){
+            headerClasses = 'header header--fixed';
+            if (scrollPos < scrollPosPrev) {
+                headerClasses += ' header--visible';
+            }
+
+            this.setState(() => ({ headerClasses }));
+        } else if (scrollPos === 0){
+            this.setState(() => ({ headerClasses: DEFAULT_HEADER_CLASSES }));
+        }
+
+        this.setState(() => ({ scrollPos }));
+    };
+
+    render() {
+        return (
+            <header id="header" className={this.state.headerClasses} onScroll={this.handleScroll}>
+                <div className="content-container">
+                    <div className="header__content">
+                        <Link className="header__title" to="/dashboard">
+                            <h1>Blogstoph</h1>
+                        </Link>
+                        {this.props.isAuthenticated ? (
+                            <div>
+                                <Link className="button button--link button--header" to="/editor/dashboard">Editor Area</Link>
+                                <button className="button button--link button--header" onClick={this.props.startLogout}>Logout</button>
+                            </div>
+                        ) : (
+                                <Link className="button button--link" to="/login">Login</Link>
+                            )}
+                    </div>
                 </div>
-            </div>
-        </header>
-    );
+            </header>
+        );
+    };
 };
 
 const mapStateToProps = (state) => ({
