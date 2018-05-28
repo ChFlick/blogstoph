e97accc7092d0b72050807d2dea5cd0f6373bb2a
Christoph Flick <christophflick@gmx.de>
2018-05-28 20:43:54 +0200

    Added style for posts preview

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,22 +2,33 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 
+const isNotLastElement = (index, numberOfPosts) => {
+    return index !== numberOfPosts - 1;
+};
+
 export const PostList = (props) => (
-    <Fragment>
+    <div className="content-container">
         {
             props.posts.length === 0 ? (
                 <p>There are no posts available</p>
             ) : (
-                props.posts.map((post) => (
-                    <Fragment key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.content}</p>
-                        <span>By {post.author} at {moment(post.date).format('DD.MM.YYYY')}</span>
-                    </Fragment>
-                ))
-            )
+                    props.posts.map((post, index) =>
+                        <Fragment key={post.id}>
+                            <div
+                                className="post-preview"
+                            >
+                                <h3 className="post-preview__title">{post.title}</h3>
+                                <p className="post-preview__subtitle">{post.content}</p>
+                                <span className="post-preview__data">
+                                    Posted by {post.author} at {moment(post.date).format('DD. MMM YYYY')}
+                                </span>
+                            </div>
+                            {isNotLastElement(index, props.posts.length) && <hr className="post-preview__separator" />}
+                        </Fragment>
+                    )
+                )
         }
-    </Fragment>
+    </div>
 );
 
 const mapStateToProps = (state) => ({
