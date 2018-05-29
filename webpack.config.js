1cf8690c4ee17792929696cde9c0927aa678ce43
Christoph Flick <christophflick@gmx.de>
2018-05-29 23:31:59 +0200

    Refactored webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,49 @@ if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.development'});
 }
 
+const getModuleRules = (CSSExtract) => [
+    {
+        loader: 'babel-loader',
+        test: /(\.js$)|(\.jsx$)/,
+        exclude: /node_modules/,
+    }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+            use: [{
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true,
+                },
+            }, {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                },
+            }]
+        })
+    }
+];
+
+const firebasePlugins = [
+    new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    }),
+];
+
+const devServer = {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    publicPath: '/dist/',
+};
+
 module.exports = (env) => {
     const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
+    const CSSExtract = new ExtractTextPlugin('styles.css')
 
     return {
         entry: ['babel-polyfill', './src/app.jsx'],
@@ -20,48 +60,15 @@ module.exports = (env) => {
             path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js',
         },
-        module: {
-            rules: [{
-                loader: 'babel-loader',
-                test: /(\.js$)|(\.jsx$)/,
-                exclude: /node_modules/,
-            }, {
-                test: /\.s?css$/,
-                use: CSSExtract.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true,
-                            },
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true,
-                            },
-                        },
-                    ],
-                }),
-            }],
-        },
         plugins: [
             CSSExtract,
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-            }),
+            ...firebasePlugins
         ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            publicPath: '/dist/',
+        module: {
+            rules: getModuleRules(CSSExtract)
         },
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer,
         resolve: {
             extensions: ['.js', '.jsx'],
         },
