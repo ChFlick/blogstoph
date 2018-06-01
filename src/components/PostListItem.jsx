859e29501e45aeeccc1fa9039fdf84ec7082d77b
Christoph Flick <christophflick@gmx.de>
2018-06-02 00:02:39 +0200

    css refactoring and styling of post preview

--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -1,20 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 const PostListItem = (props) => (
-    <Fragment>
-        <Link
-            className="post-preview"
-            to={`post/${props.post.id}`}
-        >
-            <h3 className="post-preview__title">{props.post.title}</h3>
-            <p className="post-preview__subtitle">{props.post.content}</p>
+    <div className="post-preview-item">
+        <Link className="post-preview-item__link" to={`post/${props.post.id}`} >
+            <h3 className="post-preview-item__title">{props.post.title}</h3>
+            <p className="post-preview-item__subtitle">{props.post.content}</p>
         </Link>
-        <span className="post-preview__data">
+        <span className="post-preview-item__data">
             Posted by {props.post.author} at {moment(props.post.date).format('DD. MMM YYYY')}
         </span>
-    </Fragment>
+    </div>
 );
 
 export default PostListItem;
